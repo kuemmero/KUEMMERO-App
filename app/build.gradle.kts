93ae7392d0f79e9ab0ae3343a8a8f7d110fc112c
kuemmero <markusb66@googlemail.com>
2026-09-19 17:42:47 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "de.kuemmero.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
+        versionCode = 6
         versionName = "1.0"
     }
 
