06b58bd2cb5cb1eadabf15be69d6f237d209f019
kuemmero <markusb66@googlemail.com>
2026-09-24 11:12:36 +0200

    Add files via upload

--- app/src/main/java/de/kuemmero/app/MainActivity.kt
+++ app/src/main/java/de/kuemmero/app/MainActivity.kt
@@ -2220,7 +2220,7 @@ fun KuemmeroApp() {
                 erstellePdf(
                     context, nummer, datum, gueltigBis, kunde, strasse, ort, leistung,
                     zahl(stunden), zahl(material), runde2(zahl(fahrtKm) * zahl(fahrtKostenProKm, 0.40)), zahl(stundensatz, 42.0), unterschriftPfad, unterschriftDatum,
-                    fotosVorher, fotosNachher, "ANGEBOT", 0.0, runde2(zahl(fahrtKm) * zahl(fahrtKostenProKm, 0.40)), zahl(fahrtKm), zahl(fahrtKostenProKm, 0.40)
+                    fotosVorher, fotosNachher, "ANGEBOT", 0.0, zahl(fahrtKm), zahl(fahrtKostenProKm, 0.40)
                 )
             }
             context.contentResolver.openOutputStream(uri)?.use { out -> pdf.writeTo(out) }
